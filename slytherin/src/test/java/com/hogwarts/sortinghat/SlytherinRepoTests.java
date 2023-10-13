9baa33ec4ebf14a451246de2cc20abd6b21d6f3c
libby90 <libby.hastie@ada.ac.uk>
2023-10-13 10:49:18 +0100

    Added spring and controller

---
+++ slytherin/src/test/java/com/hogwarts/sortinghat/SlytherinRepoTests.java
@@ -0,0 +1,13 @@
+package com.hogwarts.sortinghat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlytherinRepoTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
