b12ea04b15b56095081c2d07081891797b996bdf
libtreasure <146099956+libtreasure@users.noreply.github.com>
2024-05-09 16:28:24 +0100

    Add files via upload

---
+++ src/main/java/com/tbu/sortinghat/SortinghatApplication.java
@@ -0,0 +1,13 @@
+package com.tbu.sortinghat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortinghatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortinghatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tbu/sortinghat/SortinghatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbu.sortinghat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortinghatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
