805e6c116e2dc2514c617b32d8e9e229729a6ab1
Libby Treasure <elizabeth.hastie@booking.com>
2024-05-09 16:47:13 +0100

    Add REST controller

---
+++ src/main/java/com/tbu/sortinghat/controller/HatController.java
@@ -0,0 +1,16 @@
+package com.tbu.sortinghat.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/v1")
+@RestController
+public class HatController {
+    @GetMapping("/house")
+    public ResponseEntity<String> getHouse(){
+        return ResponseEntity.ok("Gryffindor!");
+    }
+
+}
