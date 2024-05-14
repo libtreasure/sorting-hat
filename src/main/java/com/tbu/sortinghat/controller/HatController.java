e04222091b137f9650fc6cad3347372a85474114
samirzafar <samir.zafar@booking.com>
2024-05-14 12:22:40 +0100

    Added service

--- src/main/java/com/tbu/sortinghat/controller/HatController.java
+++ src/main/java/com/tbu/sortinghat/controller/HatController.java
@@ -1,16 +1,24 @@
 package com.tbu.sortinghat.controller;
 
+import com.tbu.sortinghat.service.HatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/v1")
 @RestController
+@RequiredArgsConstructor
+@CrossOrigin
 public class HatController {
-    @GetMapping("/house")
-    public ResponseEntity<String> getHouse(){
-        return ResponseEntity.ok("Gryffindor!");
+
+    private final HatService hatService;
+
+    @GetMapping("/house/{name}")
+    public ResponseEntity<String> getHouse(@PathVariable String name){
+
+        String result = hatService.getHouse(name);
+
+        return ResponseEntity.ok(result);
     }
 
 }
---
+++ src/main/java/com/tbu/sortinghat/service/HatService.java
@@ -0,0 +1,10 @@
+package com.tbu.sortinghat.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HatService {
+    public String getHouse(String name) {
+        return "Gryffinsure";
+    }
+}
