570d0d460ee456e91e3c48e0b508109594d70cd1
MatthewGarrad <100134733+MatthewGarrad@users.noreply.github.com>
2024-05-16 14:26:46 +0100

    Passing part 5

--- src/main/java/com/tbu/sortinghat/service/HatService.java
+++ src/main/java/com/tbu/sortinghat/service/HatService.java
@@ -2,9 +2,25 @@ package com.tbu.sortinghat.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class HatService {
+
+    static final List<String> options = List.of("Hotelpuff!", "Ridesclaw!", "Gryffinsure!", "Slythcarin!");
+    static final List<String> darkLords = List.of("Tom", "Voldemort");
+
     public String getHouse(String name) {
-        return "Gryffinsure";
+        String house = "";
+        if (name.toLowerCase().charAt(0) == 'h') {
+            house = options.get(2);
+        } else if (darkLords.contains(name)){
+            return "Dark Lords are not permitted to work here!";
+        } else {
+            int index = (int) Math.floor(Math.random() * 4);
+            house = options.get(index);
+
+        }
+        return name + ", you are in " + house;
     }
 }
---
+++ src/test/java/com/tbu/sortinghat/service/HatServiceTest.java
@@ -0,0 +1,48 @@
+package com.tbu.sortinghat.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HatServiceTest {
+    HatService hatService;
+
+    static final List<String> options = List.of("Hotelpuff!", "Ridesclaw!", "Gryffinsure!", "Slythcarin!");
+
+    @BeforeEach
+    public void setUp() {
+        hatService = new HatService();
+    }
+
+    @Test
+    void testHouse() {
+        String result = hatService.getHouse("abv");
+        assertNotNull(result);
+        assertTrue(options.stream().anyMatch(result::contains));
+    }
+
+    @Test
+    void testPersonalisedGreeting() {
+        String result = hatService.getHouse("abv");
+        assertTrue(result.startsWith("abv, you are in"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Harry", "Homer"})
+    void testNameStartsWithH(String name) {
+        String result = hatService.getHouse(name);
+        assertEquals(result, name + ", you are in Gryffinsure!");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Tom", "Voldemort"})
+    void testDarkLords(String name) {
+        String result = hatService.getHouse(name);
+        assertEquals(result, "Dark Lords are not permitted to work here!");
+    }
+}
